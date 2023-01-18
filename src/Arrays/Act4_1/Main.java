13e7cab853ef23713d0cddae1a79a49e52832fda
dgonrom2803 <dgonrom2803@g.educaand.es>
2023-01-18 09:26:25 +0100

    Segundo commit

--- src/Arrays/Act4_1/Main.java
+++ src/Arrays/Act4_1/Main.java
@@ -1,29 +1,68 @@
 package Arrays.Act4_1;
 
-//1. Crea un método main.
+
 public class Main {
     public static void main(String[] args) {
-        //2. Declara una variable x de tipo int con valor 1
+
         int x = 1;
+        Double[] a = {1d, 2.5d, 34.56d, null, 0d};
         String s = "Hola";
+
         System.out.println(x);
-        System.out.println(s);
-        //4. Invoca el método m desde main enviando x como argumento. Comprueba si la variable x cambia
-        // de valor después de ejecutar el método. Para eso puedes imprimir la variable antes de invocar al método y después.
         m(x);
-        m(x,s);
         System.out.println(x);
+
+        System.out.println(s);
+        m(x, "Hola");
         System.out.println(s);
 
+        System.out.println(a);
+        m(x, "Hola", a);
+        System.out.println(a);
+
     }
-    //3. Crea un método m que no devuelva nada y reciba un
-    // parámetro de nombre x y tipo int. El método incremantará en una unidad el valor del parámetro.
-    public static void m (int x) {
+
+    public static void m(int x) {
         x = x + 1;
     }
-    //5. Haz lo mismo para una variable s de tipo String. Tendrás cambiar la declaración del método para
-    // que acepte un parámetro int y otro String.
-    public static void m(int x, String s){
+
+    public static void m(int x, String s) {
         s = (s.toUpperCase());
+        m(x);
     }
+
+    public static void m(int x, String s, Double[] a) {
+        a[0]++;
+        a[(a.length - 1) / 2]++;
+        a[a.length - 1]++;
+
+        m(x, s);
+
+    }
+
+    public static int[][] m2 (int x, String s, Double[] input) {
+        int n = input.length;
+        int m = 2;
+        int [][] output = new int[n][m];
+
+        m(x, s, input);
+        output[0][0] = (int)Math.floor(input[0]);
+        output[0][1] = (int)Math.floor(input[0]);
+
+        output[1][0] = (int)Math.floor(input[1]);
+        output[1][1] = (int)Math.floor(input[1]);
+
+        output[2][0] = (int)Math.floor(input[2]);
+        output[2][1] = (int)Math.floor(input[2]);
+
+        output[3][0] = (int)Math.floor(input[3]);
+        output[3][1] = (int)Math.floor(input[3]);
+
+        output[4][0] = (int)Math.floor(input[4]);
+        output[4][1] = (int)Math.floor(input[4]);
+
+
+        return output;
+    }
+
 }
