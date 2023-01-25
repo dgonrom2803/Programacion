3f87f9efbe05e8c4ab52ffd191c13ccc5b92df88
dgonrom2803 <dgonrom2803@g.educaand.es>
2023-01-25 10:27:43 +0100

    Segundo commit

---
+++ src/Arrays/ClonArray/Persona.java
@@ -0,0 +1,14 @@
+package Arrays.ClonArray;
+
+public class Persona {
+    String nombre;
+    int edad;
+
+    Persona(String nombre){
+        this.nombre = nombre;
+    }
+
+    public String toString(){
+        return nombre;
+    }
+}
---
+++ src/Arrays/CopyOfArray/EjemploArrayCopyOf.java
@@ -0,0 +1,7 @@
+package Arrays.CopyOfArray;
+
+public class EjemploArrayCopyOf {
+    public static void main(String[] args) {
+
+    }
+}
