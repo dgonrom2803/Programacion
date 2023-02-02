b586d34861beec6ed1447b68ddcb2d240d7f757e
dgonrom2803 <dgonrom2803@g.educaand.es>
2023-02-02 14:36:50 +0100

    Nueva teoría

---
+++ src/Strings/Patrones/Main.java
@@ -0,0 +1,17 @@
+package Strings.Patrones;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        Pattern p = Pattern.compile("^Pa![a-zA-Z]*");
+        Matcher m = p.matcher("Pata");
+        System.out.println(m.matches());
+
+
+        System.out.println(Arrays.toString("Hola me llamo Diego".split(" ")));
+        System.out.println("Pa".matches("^Pa"));
+    }
+}
