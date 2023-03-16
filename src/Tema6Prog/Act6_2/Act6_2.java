21bafb56342702d6b1558ba76a4647e3cd5491c9
dgonrom2803 <dgonrom2803@g.educaand.es>
2023-03-16 12:18:16 +0100

    6.2

--- src/Tema6Prog/Act6_2/Act6_2.java
+++ src/Tema6Prog/Act6_2/Act6_2.java
@@ -3,7 +3,9 @@ package Tema6Prog.Act6_2;
 public class Act6_2 {
     public static void main(String[] args) {
         cuentaAtras(5);
-        System.out.println(sumaN(5));
+        System.out.println("La suma de números naturales da: " + sumaN(5));
+        System.out.println(fibonacci(6));
+        System.out.println("El máximo común divisor de a y b es: " + maxComDiv(6,4));
     }
     private static void cuentaAtras (int i){
         if(i > 0) {
@@ -23,11 +25,22 @@ public class Act6_2 {
 
     }
 
-    private static void fibonacci (int i){
-
+    private static int fibonacci (int i){
+        if (i == 0) {
+            return 0;
+        } else if (i == 1) {
+            return 1;
+        } else {
+            return fibonacci(i-2) + fibonacci(i-1);
+        }
     }
 
-    private static void maxComDiv (int i) {
-
+    private static int maxComDiv (int a, int b) {
+        while (b != 0) {
+            int resto = a % b;
+            a = b;
+            b = resto;
+        }
+        return a;
     }
 }
