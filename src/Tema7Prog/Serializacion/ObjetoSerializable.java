1f0d94df20da63f7dfd47a0e6a652a994eb9cb86
dgonrom2803 <dgonrom2803@g.educaand.es>
2023-04-19 10:33:59 +0200

    XML

---
+++ src/Tema7Prog/Serializacion/ObjetoSerializable.java
@@ -0,0 +1,24 @@
+package Tema7Prog.Serializacion;
+
+import java.io.Serializable;
+
+public class ObjetoSerializable implements Serializable {
+
+    private static final long serialVersionUID = -4797208967054263305L;
+
+    int atributo1;
+    static int atributo2;
+    transient int atributo3;
+
+    public ObjetoSerializable(int atributo1, int atributo2, int atributo3) {
+        this.atributo1 = atributo1;
+        this.atributo2 = atributo2;
+        this.atributo3 = atributo3;
+    }
+
+    public ObjetoSerializable() {
+        this.atributo1 = 7;
+        this.atributo2 = 7;
+        this.atributo3 = 7;
+    }
+}
